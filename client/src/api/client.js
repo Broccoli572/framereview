0ff272664ea0427cb5656fbb5e05efbb2bfc702f
FrameReview Dev <dev@framereview.local>
2026-04-22 12:20:32 +0800

    fix: allow multipart asset uploads

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -7,7 +7,6 @@ const client = axios.create({
   baseURL: API_BASE_URL,
   timeout: 30000,
   headers: {
-    'Content-Type': 'application/json',
     Accept: 'application/json',
   },
 });
@@ -19,6 +18,13 @@ client.interceptors.request.use(
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
+    if (config.data instanceof FormData) {
+      delete config.headers['Content-Type'];
+    } else if (!config.headers['Content-Type']) {
+      config.headers['Content-Type'] = 'application/json';
+    }
+
     return config;
   },
   (error) => Promise.reject(error)
